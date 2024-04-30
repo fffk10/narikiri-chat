898a101d73268cee1591b092e39947d563e23142
fffk10 <warafffk10@gmail.com>
2024-04-30 17:19:11 +0900

    footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <footer>Footer</footer>
+  return (
+    <footer className='h-[40px] text-center'>
+      <p>&copy; 2024 fffk10</p>
+    </footer>
+  )
 }
