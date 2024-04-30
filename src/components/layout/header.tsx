ee4aa2efb400f6b0259e43948989feb024ce896b
fffk10 <warafffk10@gmail.com>
2024-04-30 17:18:22 +0900

    header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,3 +1,9 @@
 export default function Header() {
-  return <header>header</header>
+  return (
+    <header className='h-[70px] flex'>
+      <div className='m-auto'>
+        <p className='font-bold text-xl'>Narikiri Chat</p>
+      </div>
+    </header>
+  )
 }
