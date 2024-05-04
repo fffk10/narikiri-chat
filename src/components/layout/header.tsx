f3030c7665b2c885cf00a6d4585e2d261c415374
fffk10 <warafffk10@gmail.com>
2024-05-04 17:23:41 +0900

    header微修正

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,19 +1,8 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-
 export default function Header() {
   return (
     <header className='h-[70px] flex shadow-lg'>
       <div className='m-auto'>
         <p className='font-bold text-xl'>Narikiri Chat</p>
-      </div>
-
-      <div className='w-20 my-auto mr-4'>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
       </div>
     </header>
   )
