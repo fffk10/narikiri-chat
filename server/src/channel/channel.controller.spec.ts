f030f0e1e42e563497e486077d87f75f69d934b0
fffk10 <warafffk10@gmail.com>
2024-05-08 00:49:13 +0900

    test比較方法修正

--- server/src/channel/channel.controller.spec.ts
+++ server/src/channel/channel.controller.spec.ts
@@ -20,36 +20,33 @@ const mockRequest = {
 describe('ChannelController', () => {
   let channelService: ChannelService
 
-  beforeEach(async () => {
-    const channel: TestingModule = await Test.createTestingModule({
-      providers: [
-        {
-          provide: ChannelService,
-          useValue: {
-            createChannel: jest.fn().mockReturnValue(mockResponse),
-          },
-        },
-      ],
-    }).compile()
-
-    channelService = channel.get<ChannelService>(ChannelService)
-  })
-
   describe('正常系テスト', () => {
+    beforeEach(async () => {
+      const channel: TestingModule = await Test.createTestingModule({
+        providers: [
+          {
+            provide: ChannelService,
+            useValue: {
+              createChannel: jest.fn().mockReturnValue(mockResponse),
+            },
+          },
+        ],
+      }).compile()
+      channelService = channel.get<ChannelService>(ChannelService)
+    })
+
     it('レスポンスデータ確認', async () => {
       const controller = new ChannelController(channelService)
       const response = await controller.createChannel(mockRequest)
-      expect(JSON.stringify(response)).toBe(
-        JSON.stringify({
-          id: 'clvwj2s7e0001hgjzi63ykii7',
-          name: 'channel_name',
-          ownerId: 'user_id',
-          description: null,
-          imageUrl: null,
-          createdAt: '2024-05-07T15:10:58.635Z',
-          updatedAt: '2024-05-07T15:10:58.635Z',
-        })
-      )
+      expect(response).toEqual({
+        id: 'clvwj2s7e0001hgjzi63ykii7',
+        name: 'channel_name',
+        ownerId: 'user_id',
+        description: null,
+        imageUrl: null,
+        createdAt: '2024-05-07T15:10:58.635Z',
+        updatedAt: '2024-05-07T15:10:58.635Z',
+      })
     })
   })
 })
