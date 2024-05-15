529440c0b888a0861c0b952ea86c2f5a599b35a4
fffk10 <warafffk10@gmail.com>
2024-05-16 06:06:42 +0900

    取得するチャンネルを自身が参加しているチャンネルのみとなるように

--- packages/client/src/app/(main)/chat/[channelId]/page.tsx
+++ packages/client/src/app/(main)/chat/[channelId]/page.tsx
@@ -3,15 +3,21 @@ import axios from 'axios'
 
 import { ChannelMessageResponse } from '@/types/channel-types'
 import ChatRoom from '@/components/chat/chat-room'
+import { auth, clerkClient } from '@clerk/nextjs/server'
 
 export default async function ChannelPage({
   params,
 }: {
   params: { channelId: string }
 }) {
+  const { userId } = auth().protect()
+  const user = await clerkClient.users.getUser(userId)
+
+  if (!user) return null
+
   const url = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/v1/channel/${params.channelId}`
   const channel: Channel = await axios
-    .get(url)
+    .get(url, { params: { userId: userId } })
     .then((res) => res.data)
     .catch(() => null)
 
