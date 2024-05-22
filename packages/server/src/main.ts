3cf4650ce05bd5780ff3317b45a2b05315ff44d7
fffk10 <warafffk10@gmail.com>
2024-05-22 20:00:04 +0900

    swaggerセットアップ

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,5 +1,6 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core'
 import { PrismaClientExceptionFilter } from 'nestjs-prisma'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 
@@ -7,6 +8,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   // CORS有効化
   app.enableCors()
+
+  // OpenAPI
+  const config = new DocumentBuilder()
+    .setTitle('title')
+    .setDescription('Swagger UI の説明文')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
   // Prismaエラーハンドリング用
   const { httpAdapter } = app.get(HttpAdapterHost)
