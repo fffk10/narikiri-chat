41732577180d42902c856cde7e59484e72d19c32
fffk10 <warafffk10@gmail.com>
2024-05-23 22:20:02 +0900

    リクエストパラム仕様

--- packages/server/src/member/member.controller.ts
+++ packages/server/src/member/member.controller.ts
@@ -1,6 +1,7 @@
 import { MemberService } from '@/member/member.service'
 import { Controller, Get, Param } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger'
+import { channel } from 'diagnostics_channel'
 
 @Controller('v1/member')
 @ApiTags('/member')
@@ -8,6 +9,12 @@ export class MemberController {
   constructor(private readonly memberService: MemberService) {}
 
   @Get(':channelId')
+  @ApiOperation({ summary: 'チャンネルに所属するメンバー一覧取得' })
+  @ApiParam({
+    name: 'channelId',
+    type: String,
+    example: 'test-channel-id',
+  })
   async getMembers(@Param('channelId') channelId: string) {
     return this.memberService.getMembers(channelId)
   }
