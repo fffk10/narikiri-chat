1243f5f77edce85bc14a1b7f1ccaf0aa8e8746b9
fffk10 <warafffk10@gmail.com>
2024-05-31 21:25:03 +0900

    チャンネルリストにチャンネルイメージを追加

--- packages/client/src/components/chat/channel-list-item.tsx
+++ packages/client/src/components/chat/channel-list-item.tsx
@@ -1,4 +1,5 @@
 import { Channel } from '@prisma/client'
+import Image from 'next/image'
 
 type ChannelListItem = {
   channel: Channel
@@ -8,8 +9,15 @@ type ChannelListItem = {
 export default function ChannelListItem({ channel, onClick }: ChannelListItem) {
   return (
     <li key={channel.id} className='mb-2'>
-      <button onClick={onClick} className='text-left text-sm w-full'>
-        {channel.name}
+      <button onClick={onClick} className='flex gap-2 text-left text-sm w-full'>
+        <Image
+          src={channel.imageUrl || '/user.svg'}
+          alt='channel image'
+          width={40}
+          height={40}
+          className='rounded-full border'
+        />
+        <p className='my-auto'>{channel.name}</p>
       </button>
     </li>
   )
